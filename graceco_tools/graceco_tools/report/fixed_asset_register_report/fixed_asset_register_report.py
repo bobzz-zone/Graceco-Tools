1a4d6ac82962c075c8e71bcaf7cbeb92f1e32ae2
bobzz-zone <bobzz.zone@gmail.com>
2017-01-29 13:42:15 +0700

    fixed asset report

--- graceco_tools/graceco_tools/report/fixed_asset_register_report/fixed_asset_register_report.py
+++ graceco_tools/graceco_tools/report/fixed_asset_register_report/fixed_asset_register_report.py
@@ -6,10 +6,10 @@ import frappe
 
 def execute(filters=None):
 	#Item Code	Asset Name	Asset Catergory	Warehouse	Purchase Date	Value (After Depreciation)
-	columns, data = ["Item Code:Link/Item:200","Asset Name::200","Asset Category:Link/Asset Category:200","Warehouse:Link/Warehouse:200","Purchase Date:Date:200","Book Value:Currency:200"], []
+	columns, data = ["Item Code:Link/Item:150","Asset Name::150","Asset Category:Link/Asset Category:150","Warehouse:Link/Warehouse:150","Purchase Date:Date:150","Book Value:Currency:200","Gross Purchase Amount:Currency:200"], []
 	where=""
 	if filters.get("asset_category"):
 		where = """ and asset_category = "{}" """.format(filters.get("asset_category"))
-	data = frappe.db.sql("""select item_code,name,asset_category,warehouse,purchase_date,value_after_depreciation 
+	data = frappe.db.sql("""select item_code,name,asset_category,warehouse,purchase_date,value_after_depreciation,gross_purchase_amount 
 		from tabAsset where docstatus=1 {}""".format(where),as_list=1)
 	return columns, data
